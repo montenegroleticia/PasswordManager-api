984006062357ac0dcfe367072d8177dd2a06df90
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-09-04 14:47:26 -0300

    Prisma test config

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,16 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaModule } from '../src/prisma/prisma.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, PrismaModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prisma = app.get(PrismaService);
+
+    await prisma.card.deleteMany();
+    await prisma.credential.deleteMany();
+    await prisma.note.deleteMany();
+    await prisma.user.deleteMany();
     await app.init();
   });
 
